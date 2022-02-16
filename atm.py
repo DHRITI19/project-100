485d3323e1fe631f07df9ea7e784e602773e592a
DHRITI19 <76477697+DHRITI19@users.noreply.github.com>
2022-02-16 19:18:15 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,18 @@
+class Atm(object)
+
+
+
+
+    def __init__(self, card, card_holder_name, bank, card_pin):
+      self.card = card
+      self.card_holder_name = card_holder_name
+      self.bank = bank
+      self.card_pin = car_pin
+
+    def withdrawal(self):
+      print("withdrawed")
+
+    def balance_amount(self):
+      print("balance_amount")
+
+   
